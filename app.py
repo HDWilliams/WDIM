7e14116c56b808038da0f0664d826a33acd35dff
HDWilliams <hughdaywilliams@gmail.com>
2023-06-11 16:53:06 -0700

    added success route for OAuth

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello_world():
     return 'It Works'
 
+@app.route('/success')
+def success():
+    return "Successfully Authenticated, you may now close this tab and return to Slack"
+
 if __name__ == '__main__':
     app.run()
