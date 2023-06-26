d1a27fc20cd4f5fc5a982c0b26d54999be9796ce
HDWilliams <hughdaywilliams@gmail.com>
2023-06-25 17:30:26 -0700

    first attempt at logging conversation history

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from slack import WebClient
+import requests
+import os
+
+#environ variables
+api_key = os.getenv("api_key")
+
+client = WebClient(token=api_key)
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'It Works'
+
+@app.route('/summarize', methods = ['POST'])
+def summarize():
+    response_url = request.form['response_url']
+    channel_id = request.form['channel_id']
+    requests.post(response_url, json={'text': 'Processing...'})
+
+    #get channel history
+    response = client.conversations_history(channel=channel_id, limit=100)
+    print(response)
+
+
 
 @app.route('/success')
 def success():
